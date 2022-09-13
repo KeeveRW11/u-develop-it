a513bf004a144e2f11394861618e0074a0c9b99c
KeeveRW11 <keevewhyte+github@gmail.com>
2022-09-12 22:34:48 -0400

    test connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql =require('mysql2');
 const exp = require('constants');
 const express = require('express');
 
@@ -7,6 +8,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connects to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'go-hoM3!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
